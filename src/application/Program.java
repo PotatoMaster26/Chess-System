3aa130aa8806d5f19c845580538a0b8f42d97824
Mauri Junior <mvrjunior26@gmail.com>
2024-03-22 16:57:37 -0300

    This took too long

---
+++ src/application/Program.java
@@ -0,0 +1,11 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("This took way too long to get started, but not because i didn't try");
+	}
+
+}
