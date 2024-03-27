1895d110afecf085aae76fdd20c10c3c031dc064
Mauri Junior <mvrjunior26@gmail.com>
2024-03-27 14:45:46 -0300

    Added piece move count(not visible)

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -7,6 +7,7 @@ import boardGame.Position;
 public abstract class ChessPiece extends Piece {
 
 	private Color color;
+	private int moveCount;
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
@@ -17,6 +18,13 @@ public abstract class ChessPiece extends Piece {
 		return color;
 	}
 
+	public void increaseMoveCount() {
+		moveCount++;
+	}
+	public void decreaseMoveCount() {
+		moveCount--;
+	}
+	
 	public ChessPosition getChessPosition() {
 		return ChessPosition.fromPosition(position);
 	}
