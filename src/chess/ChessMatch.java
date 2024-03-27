9af48ddeb6fad9d7a025ce5175c8cdf6117a7c21
Mauri Junior <mvrjunior26@gmail.com>
2024-03-27 15:35:59 -0300

    Added bishop and bishop movement

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -7,6 +7,7 @@ import java.util.stream.Collectors;
 import boardGame.Board;
 import boardGame.Piece;
 import boardGame.Position;
+import chess.pieces.Bishop;
 import chess.pieces.King;
 import chess.pieces.Pawn;
 import chess.pieces.Rook;
@@ -192,8 +193,13 @@ public class ChessMatch {
 	}
 
 	private void initialSetup() {
-		placeNewPiece('e', 1, new King(board, Color.WHITE));
 		placeNewPiece('a', 1, new Rook(board, Color.WHITE));
+		//knight
+		placeNewPiece('c', 1, new Bishop(board, Color.WHITE));
+		//queen
+		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('f', 1, new Bishop(board, Color.WHITE));
+		//knight
 		placeNewPiece('h', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('a', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
@@ -204,8 +210,13 @@ public class ChessMatch {
 		placeNewPiece('g', 2, new Pawn(board, Color.WHITE));
 		placeNewPiece('h', 2, new Pawn(board, Color.WHITE));
 
-		placeNewPiece('e', 8, new King(board, Color.BLACK));
 		placeNewPiece('a', 8, new Rook(board, Color.BLACK));
+		//knight
+		placeNewPiece('c', 8, new Bishop(board, Color.BLACK));
+		//queen
+		placeNewPiece('e', 8, new King(board, Color.BLACK));
+		placeNewPiece('f', 8, new Bishop(board, Color.BLACK));
+		//knight
 		placeNewPiece('h', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 		placeNewPiece('b', 7, new Pawn(board, Color.BLACK));
